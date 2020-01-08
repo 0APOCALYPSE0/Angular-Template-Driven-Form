3ce7f5f6551c2f72590dfb702b98c3c4373893b6
unknown <giriaakash00@gmail.com>
2020-01-08 15:24:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-template-driven-form';
+  topics = ['Angular', 'React', 'Vue'];
+  topicHasError = true;
+  submitted = false;
+  errorMsg = '';
+
+  constructor (private enrollmentService: EnrollmentService) { }
+  userModel: User = new User('Aakash Giri', 'giriaakash00@gmail.com', 8368325809, 'default', 'morning', true);
+
+  validateTopic(value){
+    if(value === 'default'){
+      this.topicHasError = true;
+    }else{
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    this.enrollmentService.enroll(this.userModel).subscribe(
+      data => console.log('Success!', data),
+      error => this.errorMsg = error.statusText
+    );
+  }  
+
 }
